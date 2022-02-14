6f19f785c9f4a703dbab0af5a47e74361b20b09d
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-02-14 09:57:39 -0700

    Added the first practice "Hello World"

---
+++ EVA1_1_HOLAMUNDO/src/Eva1_1_HolaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jorge Eduardo Escobar Bugarini
+ */
+
+/*
+    Palabras en azul : Palabras reservadas y de uso exclusivo del lenguaje.
+    Palabras en negro: Identificadores → variables, constantes, clases, funciones, objetos, etc.
+    []: arreglos
+    {}: delimitadores → bloques de codigo.
+*/
+public class Eva1_1_HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
