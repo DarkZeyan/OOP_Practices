3b132cc5171cfba6add3ad82f82d80919bed4bd0
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-03-13 18:45:28 -0700

    Add files via upload

---
+++ EVA_1_13_ACCESS_MODIFIERS/src/Vehiculos_Sin_Motor/Bicicleta.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vehiculos_Sin_Motor;
+
+/**
+ *
+ * @author user
+ */
+public class Bicicleta {
+    private String rodada;
+    private String marca;
+    private String año;
+    
+
+    public String getRodada() {
+        return rodada;
+    }
+
+    public void setRodada(String rodada) {
+        this.rodada = rodada;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+}
---
+++ EVA_1_13_ACCESS_MODIFIERS/src/eva_1_13_access_modifiers/EVA_1_13_ACCESS_MODIFIERS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_1_13_access_modifiers;
+import  Vehiculos_Sin_Motor.*;
+
+/**
+ *
+ * @author user
+ */
+public class EVA_1_13_ACCESS_MODIFIERS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Automovil miCarro = new Automovil();
+        miCarro.setAño(2011);
+        miCarro.setMarca("Chevrolet");
+        miCarro.setModelo("Cruze");
+        Bicicleta bici = new Bicicleta();
+        Carreta miCarreta = new Carreta();
+        
+    }
+    
+}
+
