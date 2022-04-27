38f723a681f0eb389bc9b13ab295129439f21fee
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-04-27 09:46:23 -0600

    Update EVA2_16_FIGURAS.java

--- EVA2_16_FIGURAS/src/eva2_16_figuras/EVA2_16_FIGURAS.java
+++ EVA2_16_FIGURAS/src/eva2_16_figuras/EVA2_16_FIGURAS.java
@@ -21,12 +21,30 @@ public class EVA2_16_FIGURAS {
         Rectangulo rectangulin = new Rectangulo(5.0,3.0);
         Triangulo triangulin = new Triangulo(10.0,2.0);
         
+        
+        //Para el polimorfismo verificar la relación IS-A
         imprimirArea(triangulin);
         imprimirArea(circulin);
-    
+        imprimirArea(rectangulin);
+        
+        claseObject(triangulin);
+ 
     }
     
     public static void imprimirArea(Figura f){
         System.out.println("Area = "+f.calcularArea());
+        //RESTAURAR EL OBJETO: 
+        //Convertir un tipo de dato a otro → Casting
+        
+        if(f instanceof Triangulo){
+            System.out.println(f.getClass());
+         Triangulo trian = (Triangulo)f; //Convertir la figura devuelta a un triangulo
+        }
+         
+    }
+    
+    
+    public static void claseObject(Object o){
+        System.out.println(o.getClass());
     }
 }
