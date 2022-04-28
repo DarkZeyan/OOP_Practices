f2255de91c70510afe8d3fb2af0f434ed0c511a3
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-04-28 09:43:32 -0600

    Practicas realizadas durante el 28/04/22

---
+++ EVA2_18_INTERFACES_OBJ/src/eva2_18_interfaces_obj/EVA2_18_INTERFACES_OBJ.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_18_interfaces_obj;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_18_INTERFACES_OBJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        //Mensajes mensaje = new Mensaje();
+        
+        
+        Persona persona = new Persona();
+        persona.setNombre("Jorge");
+        
+        Mensajes mensaje = persona;
+        
+        mensaje.mostrarMensaje();
+        
+    }
+    
+}
+
+interface Mensajes{
+    public void mostrarMensaje();
+}
+
+class Persona implements Mensajes{
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    @Override
+    public void mostrarMensaje(){
+        System.out.println("Nombre: "+nombre);
+    }
+    
+    
+    
+}
