b00fe4affaca92d26f52d14f8c48286e46450b2a
Jorge Bugarini <64108165+DarkZeyan@users.noreply.github.com>
2022-05-24 09:52:25 -0600

    Add files via upload

---
+++ EVA3_18_ARCHIVOS/src/eva3_18_archivos/EVA3_18_ARCHIVOS.java
@@ -0,0 +1,47 @@
+
+
+package eva3_18_archivos;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_18_ARCHIVOS {
+
+    /*
+    Archivos -→ Almacenamiento de información, utilizaremos archivos de tipo texto y archivos binarios.
+    
+    Stream → Torrente → Flujo de bytes
+    
+    */
+    
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            
+            //Ruta:
+            //FileInputStream funciona para leer los archivos de texto.
+            FileInputStream file = new FileInputStream("C:/POOFiles/archivo1.txt");
+            int caracter = file.read();
+            do{
+                
+                System.out.print((char)caracter);
+                caracter=file.read();
+            }
+            while(caracter!=-1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("El archivo no existe");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+}
